da2d81b0ab8e3448825d8f941f595f0ee6dbdfbf
Saenyakorn Siangsanoh <saenyakorn.si@gmail.com>
2022-06-29 21:50:44 +0700

    fix discord schema

--- prisma/migrations/20220629144604_/migration.sql
+++ prisma/migrations/20220629144604_/migration.sql
@@ -5,27 +5,28 @@ CREATE TYPE "BranchType" AS ENUM ('PROGRAMMING', 'MARKETING', 'CONTENT', 'DESIGN
 CREATE TABLE "Camper" (
     "id" UUID NOT NULL DEFAULT gen_random_uuid(),
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" TIMESTAMP(3) NOT NULL,
-    "email" TEXT NOT NULL,
-    "camperId" TEXT NOT NULL,
-    "discordId" TEXT NOT NULL,
-    "firebaseId" TEXT NOT NULL,
-    "firstName" TEXT NOT NULL,
-    "firstNameEn" TEXT NOT NULL,
-    "lastName" TEXT NOT NULL,
-    "lastNameEn" TEXT NOT NULL,
-    "nickname" TEXT NOT NULL,
-    "telephone" TEXT NOT NULL,
-    "branch" "BranchType" NOT NULL,
-    "points" INTEGER NOT NULL DEFAULT 0,
-    "emergencyFirstName" TEXT NOT NULL,
-    "emergencyLastName" TEXT NOT NULL,
-    "emergencyTelephone" TEXT NOT NULL,
-    "emergencyRelationship" TEXT NOT NULL,
-    "parentFirstName" TEXT NOT NULL,
-    "parentLastName" TEXT NOT NULL,
-    "parentTelephone" TEXT NOT NULL,
-    "parentRelationship" TEXT NOT NULL,
+    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "email" TEXT,
+    "camperId" TEXT,
+    "discordId" TEXT,
+    "firebaseId" TEXT,
+    "citizenId" TEXT,
+    "firstName" TEXT,
+    "firstNameEn" TEXT,
+    "lastName" TEXT,
+    "lastNameEn" TEXT,
+    "nickname" TEXT,
+    "telephone" TEXT,
+    "branch" "BranchType",
+    "points" INTEGER DEFAULT 0,
+    "emergencyFirstName" TEXT,
+    "emergencyLastName" TEXT,
+    "emergencyTelephone" TEXT,
+    "emergencyRelationship" TEXT,
+    "parentFirstName" TEXT,
+    "parentLastName" TEXT,
+    "parentTelephone" TEXT,
+    "parentRelationship" TEXT,
     "healthAllergicDrug" TEXT,
     "healthAllergicThing" TEXT,
     "healthCongenitalDisease" TEXT,
@@ -33,14 +34,7 @@ CREATE TABLE "Camper" (
     "healthDrug" TEXT,
     "teamId" UUID,
 
-    CONSTRAINT "Camper_pkey" PRIMARY KEY ("id","discordId","firebaseId","camperId")
-);
-
--- CreateTable
-CREATE TABLE "UnregisteredCamper" (
-    "firebaseId" TEXT NOT NULL,
-
-    CONSTRAINT "UnregisteredCamper_pkey" PRIMARY KEY ("firebaseId")
+    CONSTRAINT "Camper_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
@@ -64,6 +58,9 @@ CREATE UNIQUE INDEX "Camper_discordId_key" ON "Camper"("discordId");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "Camper_firebaseId_key" ON "Camper"("firebaseId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Camper_citizenId_key" ON "Camper"("citizenId");
 
 -- AddForeignKey
 ALTER TABLE "Camper" ADD CONSTRAINT "Camper_teamId_fkey" FOREIGN KEY ("teamId") REFERENCES "Team"("id") ON DELETE SET NULL ON UPDATE CASCADE;
