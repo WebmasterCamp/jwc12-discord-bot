a4a0f3550e956f8646ce465539cd6fbde072b4bd
Suphon Thanakornpakapong <paphonb@gmail.com>
2022-06-30 19:43:03 +0700

    Implement verify command

--- src/discord-bot/commands/verify/verify.command.ts
+++ src/discord-bot/commands/verify/verify.command.ts
@@ -1,15 +1,18 @@
 import { Injectable, Logger } from '@nestjs/common'
 
-import { Command, DiscordCommand } from '@discord-nestjs/core'
+import { Command, DiscordCommand, On, UseGuards } from '@discord-nestjs/core'
 import {
   CommandInteraction,
   InteractionReplyOptions,
   MessageActionRow,
   Modal,
   ModalActionRowComponent,
+  ModalSubmitInteraction,
   TextInputComponent,
 } from 'discord.js'
 import { TextInputStyles } from 'discord.js/typings/enums'
+import { IsModalInteractionGuard } from 'src/discord-bot/guard'
+import { capitalize } from 'src/discord-bot/utils'
 import { PrismaService } from 'src/prisma.service'
 
 @Command({
@@ -19,40 +22,81 @@ import { PrismaService } from 'src/prisma.service'
 @Injectable()
 export class VerifyCommand implements DiscordCommand {
   private readonly logger = new Logger(VerifyCommand.name)
+  private readonly verifyModalId = 'Verify'
+  private readonly verifyCodeComponentId = 'VerifyCode'
 
   constructor(private prisma: PrismaService) {
     this.logger.log(`${VerifyCommand.name} initialized`)
   }
 
   async handler(interaction: CommandInteraction): Promise<InteractionReplyOptions | void> {
-    const modal = new Modal().setTitle('Request participation').setCustomId('modal')
+    const account = await this.prisma.discordAccount.findUnique({
+      select: { discordId: true },
+      where: {
+        discordId: interaction.user.id,
+      },
+    })
+    if (account) {
+      this.logger.error(`User ${interaction.user.id} is already registered`)
+      return { content: 'คุณได้ยืนยันตัวตนไปแล้ว', ephemeral: true }
+    }
+    const modal = new Modal().setTitle('ยืนยันตัวตน').setCustomId(this.verifyModalId)
 
-    const userNameInputComponent = new TextInputComponent()
-      .setCustomId('username')
-      .setLabel('Your username')
+    const verifyCodeInputComponent = new TextInputComponent()
+      .setCustomId(this.verifyCodeComponentId)
+      .setLabel('รหัสยืนยันตัวตน')
       .setStyle(TextInputStyles.SHORT)
 
-    const commentInputComponent = new TextInputComponent()
-      .setCustomId('input')
-      .setLabel('Add an explanatory comment')
-      .setStyle(TextInputStyles.PARAGRAPH)
-
-    const rows = [userNameInputComponent, commentInputComponent].map((component) =>
+    const rows = [verifyCodeInputComponent].map((component) =>
       new MessageActionRow<ModalActionRowComponent>().addComponents(component)
     )
 
     modal.addComponents(...rows)
 
     await interaction.showModal(modal)
+  }
 
-    // const camperInfo = await this.prisma.camper.findUnique({
-    //   select: { points: true },
-    //   where: { discordId: interaction.user.id },
-    // })
+  @On('interactionCreate')
+  @UseGuards(IsModalInteractionGuard)
+  async onModuleSubmit(modal: ModalSubmitInteraction) {
+    this.logger.log(`Modal ${modal.customId} submit`)
 
-    // if (!camperInfo) {
-    //   this.logger.error(`User ${interaction.user.id} is not registered`)
-    //   return { content: 'หาข้อมูลของคุณไม่พบ' }
-    // }
+    if (modal.customId !== this.verifyModalId) return
+
+    const verifyCode = modal.fields.getTextInputValue(this.verifyCodeComponentId)
+    if (verifyCode.length !== 6) {
+      await modal.reply({ content: 'รหัสยืนยันตัวตนไม่ถูกต้อง', ephemeral: true })
+      return
+    }
+
+    const camper = await this.prisma.camper.findFirst({
+      select: { id: true, nickname: true, branch: true },
+      where: {
+        firebaseId: {
+          startsWith: verifyCode,
+        },
+      },
+    })
+    if (camper === null) {
+      await modal.reply({ content: 'รหัสยืนยันตัวตนไม่ถูกต้อง', ephemeral: true })
+      return
+    }
+
+    await this.prisma.camper.update({
+      data: {
+        discordAccounts: {
+          create: {
+            discordId: modal.user.id,
+          },
+        },
+      },
+      where: {
+        id: camper.id,
+      },
+    })
+    await modal.reply({
+      content: `ยินดีต้อนรับ น้อง${camper.nickname} สาขา ${capitalize(camper.branch)}`,
+      ephemeral: true,
+    })
   }
 }
